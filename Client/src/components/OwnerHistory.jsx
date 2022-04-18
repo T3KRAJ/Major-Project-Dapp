5963275574a536ed08fc97cf945319f3315a2cf7
GaneshPandey-GP <ganeshpandey897@gmail.com>
2022-04-18 17:16:28 +0530

    owner-history

--- Client/src/components/OwnerHistory.jsx
+++ Client/src/components/OwnerHistory.jsx
@@ -1,41 +1,34 @@
 import React from 'react'
 
 const OwnerHistory = ({ owners }) => {
+ 
     return (
-        <div className="container mx-auto w-full">
-            <div className="relative wrap overflow-hidden p-10">
-                <div className="border-2-2 absolute border-opacity-20 border-white h-full border" style={{ left: "50%" }}></div>
-                {
-                    owners && owners.map((owner, index) => (
-                        <div className="mb-8 flex justify-between items-center w-full right-timeline">
-                            <div className="order-1 w-5/12"></div>
-                            <div className="z-20 flex items-center order-1 bg-gray-800 shadow-xl w-8 h-8 rounded-full">
-                                <h1 className="mx-auto font-semibold text-lg text-white">{index + 1}</h1>
-                            </div>
-                            <div className="order-1 bg-gray-400 rounded-lg shadow-xl w-5/12 px-6 py-4 ">
-                                <h3 className="mb-3 font-bold text-white text-normal">{owner.owner}</h3>
-                                <p className="text-sm whitespace-nowrap leading-snug tracking-wide text-white text-opacity-100">{owner.timestamp.toString()}</p>
-                            </div>
-                        </div>
-                    ))
-                }
 
-                {/* <div className={"mb-8 flex justify-between items-center w-full right-timeline" + index % 2 == 0 ? "flex-row-reverse" : ''}> */}
-
-                <div className="mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline">
-                    <div className="order-1 w-5/12"></div>
-                    <div className="z-20 flex items-center order-1 bg-gray-800 shadow-xl w-8 h-8 rounded-full">
-                        <h1 className="mx-auto text-white font-semibold text-lg">2</h1>
-                    </div>
-                    <div className="order-1 bg-red-400 rounded-lg shadow-xl w-5/12 px-6 py-4">
-                        <h3 className="mb-3 font-bold text-white text-xl">Lorem Ipsum</h3>
-                        <p className="text-sm font-medium leading-snug tracking-wide text-white text-opacity-100">Lorem Ipsum</p>
-                    </div>
-                </div>
-
+        <div class="container bg-gray-600 mx-auto w-full h-full">
+          <div class="relative wrap overflow-hidden p-10 h-full">
+            <div class="border-2-2 absolute border-opacity-20 border-white-700 h-full border" style={{left: "50%"}}></div>
+        { owners && owners.map((owner, index) => {
+          const timeline = index % 2 === 0 ? " right-timeline" : " left-timeline flex-row-reverse"
+          return(
+      <div class={`mb-8 flex justify-between items-center w-full ${timeline} `}>
+              <div class="order-1 w-5/12 text-white-200"></div>
+              <div class="z-20 flex items-center order-1 bg-gray-800 shadow-xl w-8 h-8 rounded-full">
+                <h1 class="mx-auto font-semibold text-lg text-white">{index+1}</h1>
+              </div>
+              <div class="order-1 bg-gray-400 rounded-lg shadow-xl w-5/12 px-6 py-4">
+                <h4 class="mb-3 font-bold text-gray-800 text-sm">{owner.owner}</h4>
+                <p class="text-sm leading-snug tracking-wide text-gray-900 text-opacity-100">{owner.timestamp.toString()}</p>
+              </div>
             </div>
+
+            
+        )})
+            
+            }
+          </div>
         </div>
     )
+
 }
 
 export default OwnerHistory
